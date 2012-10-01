5e6459834ae96ea513f51ec58f4c73f141ab7e10
LouisBanvillet <Louis@130.66.204.223>
2012-10-01 17:10:31 +0200

    pu

--- src/Loft.java
+++ src/Loft.java
@@ -6,6 +6,7 @@ public class Loft {
 	protected int w, h;
 	protected ObjetLoft loftTable[][] = new ObjetLoft[w][h];
 	protected ArrayList<Neuneu> population = new ArrayList<Neuneu>();
+	protected ArrayList<Aliment> listeAliment = new ArrayList<Aliment>();
 	
 	public Loft(){
 		this(10, 10);
@@ -15,15 +16,18 @@ public class Loft {
 		w=width;
 		h=height;
 		int numeroNeuneu=1;
+		int numeroAliment=1;
 		for(int i=0; i<w; i++) {
 	        for(int j=0; j<h; j++) {
 	        	//random<3 : case vide; 3<=random<5 : aliment; 5<=random<6 : lofteur
 	        	int random = (int)(Math.random() * (6));
 	        	if(random>=3 && random<5){
-	        		loftTable[i][j] = AlimentFactory.createAliment();
+	        		loftTable[i][j] = AlimentFactory.createAliment("Aliment" + numeroAliment);
+	        		listeAliment.add((Aliment) loftTable[i][j]);
+	        		numeroAliment++;
 	        	}
 	        	if(random>=5 && random<6){
-	        		loftTable[i][j] = NeuneuFactory.createNeuneu("Neuneu" + numeroNeuneu);
+	        		loftTable[i][j] = NeuneuFactory.createNeuneu("Neuneu" + numeroNeuneu, this);
 	        		population.add((Neuneu) loftTable[i][j]);
 	        		numeroNeuneu++;
 	        	}
@@ -31,21 +35,85 @@ public class Loft {
 		}
 	}
 	
-	public Neuneu getProcheNeuneu(Neuneu n){
+	
+	//méthode régissant la partie
+	public void go(){
+		while(population.size()>0){
+			for(Neuneu n : population){
+				n.seDeplacer();
+			}
+			int incrementNeuneu=0;
+			while(incrementNeuneu<population.size()){
+				population.get(incrementNeuneu).action();
+				if(population.get(incrementNeuneu).getEnergie()<=0){
+					population.remove(incrementNeuneu);
+				}
+				else{incrementNeuneu++;}
+			}
+			int incrementAliment=0;
+			while(incrementAliment<listeAliment.size()){
+				if(listeAliment.get(incrementAliment).getEnergie()<=0){
+					listeAliment.remove(incrementAliment);
+				}
+			}
+		}
+	}
+	
+	
+	//renvoie le neuneu le plus proche de l'objet en paramètre
+	public Neuneu getProcheNeuneu(ObjetLoft o){
 		Neuneu procheNeuneu = null;
 		
 		for(int i=0; i<population.size(); i++){
 			//si la distance est nulle, on ne considère pas ce Neuneu
-			if(n.getDistance(population.get(i))!=0){
+			if(o.getDistance(population.get(i))!=0){
 				if(procheNeuneu==null){procheNeuneu=population.get(i);}
-				else if(n.getDistance(population.get(i))<n.getDistance(procheNeuneu)){
+				else if(o.getDistance(population.get(i))<o.getDistance(procheNeuneu)){
 					procheNeuneu=population.get(i);
 				}
 			}
 		}
 		
-		return null;
+		return procheNeuneu;
 	}
+
+	//renvoie l'objet le plus proche de l'objet en paramètre
+	public Aliment getProcheAliment(ObjetLoft o){
+		Aliment procheObjet = null;
+		
+		//puis la population d'objet		
+		for(int i=0; i<listeAliment.size(); i++){
+			//si la distance est nulle, on ne considère pas cet objet
+			if(o.getDistance(listeAliment.get(i))!=0){
+				if(procheObjet==null){procheObjet=listeAliment.get(i);}
+				else if(o.getDistance(listeAliment.get(i))<o.getDistance(procheObjet)){
+					procheObjet=listeAliment.get(i);
+				}
+			}
+		}
+		
+		return procheObjet;
+	}
+	
+	//renvoie l'objet le plus proche de l'objet en paramètre
+		public ObjetLoft getProcheObjet(ObjetLoft o){
+			ObjetLoft procheObjet = null;
+			
+			if(getProcheNeuneu(o)!=null){
+				procheObjet=getProcheNeuneu(o);
+			}
+			
+			if(getProcheAliment(o)!=null){
+				if(getProcheNeuneu(o)!=null){
+					if(o.getDistance(getProcheAliment(o))<o.getDistance(getProcheNeuneu(o))){
+						procheObjet=getProcheAliment(o);						
+					}
+				}
+				else{procheObjet=getProcheAliment(o);}
+			}			
+			
+			return procheObjet;
+		}
 	
 	
 	//getters et setters
